3be42c337644ae9856cae82157c296ab880353d8
Md. Faruk Hossain <56917327+programmerfaruk@users.noreply.github.com>
2023-08-21 09:38:50 +0600

    files added from desktop

---
+++ script.js
@@ -0,0 +1,9 @@
+const totalPrice = document.getElementById('total-Price');
+const totalPriceInText = totalPrice.innerText;
+const price = parseFloat(totalPriceInText);
+
+
+function updatePrice(){
+    const 
+
+}
